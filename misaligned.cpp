2d8a756d161eeef7bc2bafe9bb00e13d982b6abd
arpit091429 <arpitkhandelwal24@gmail.com>
2022-04-28 11:04:19 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,31 @@
-#include <iostream>
 #include <assert.h>
+#include <iostream>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-        }
-    }
-    return i * j;
+void checkPairNumberValidity(int i, int j)
+{
+    int pair_number = i * 5 + j;
+    std::cout << pair_number + 1;
+    assert(pair_no >= 1 && pair_no <= 25);
 }
 
-int main() {
-    int result = printColorMap();
-    assert(result == 25);
+void printColorMap()
+{
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    int         i = 0, j = 0;
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            checkPairNumberValidity(i,j);
+        }
+    }
+}
+
+int main()
+{
+    printColorMap();
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
