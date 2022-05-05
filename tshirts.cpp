6d1171d90cc08b79451d9cf51911d6806d5d111c
arpit091429 <arpitkhandelwal24@gmail.com>
2022-05-05 12:37:06 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,20 +1,28 @@
-#include <iostream>
 #include "TestTshirtSize.h"
+#include <iostream>
 
-char size(int cms) {
+char size(int cms)
+{
     char sizeName = '\0';
-    if(cms <= 38) {
+    if (cms <= 38)
+    {
         sizeName = 'S';
-    } else if(cms > 38 && cms <= 42) {
+    }
+    else if (cms > 38 && cms <= 42)
+    {
         sizeName = 'M';
-    } else if(cms > 42) {
+    }
+    else if (cms > 42)
+    {
         sizeName = 'L';
     }
     return sizeName;
 }
 
-int main() {
-    testTshirtsSize();
+int main()
+{
+    testTshirtsSize(size);
     std::cout << "All is well (maybe!)\n";
+    getchar();
     return 0;
 }
