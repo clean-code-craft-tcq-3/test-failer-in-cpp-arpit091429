ea64defccce3135922a1979089e080795be690d6
arpit091429 <arpitkhandelwal24@gmail.com>
2022-05-05 19:20:08 +0530

    Update misaligned.h

--- misaligned.h
+++ misaligned.h
@@ -1,25 +1,42 @@
-#include <iostream>
+#ifndef MISALIGNED_H
+#define MISALIGNED_H
+
 #include <assert.h>
+#include <iostream>
 
-enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
-enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+enum MajorColor
+{
+    WHITE,
+    RED,
+    BLACK,
+    YELLOW,
+    VIOLET
+};
+enum MinorColor
+{
+    BLUE,
+    ORANGE,
+    GREEN,
+    BROWN,
+    SLATE
+};
 
-extern const char* MajorColorNames[];
-extern const char* MinorColorNames[];
-extern int numberOfMajorColors;
-extern int numberOfMinorColors;
+const char* MajorColorNames[];
+const char* MinorColorNames[];
 
 class ColorPair
 {
-private:
+  private:
     MajorColor m_majorColor;
     MinorColor m_minorColor;
-public:
-    ColorPair(MajorColor major, MinorColor minor);      
-    MajorColor getMajor();
-    MinorColor getMinor();
+
+  public:
+    ColorPair(MajorColor major, MinorColor minor);
+    MajorColor  getMajor();
+    MinorColor  getMinor();
     std::string ToString();
 };
 ColorPair ConvertPairNumberstoColor(int pairNumber);
-void printColorCodeMap();
+void      printColorCodeMap();
 
+#endif
